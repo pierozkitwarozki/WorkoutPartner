903d6ee0e75029067a41d6cffaaffc330629ae48
Bartłomiej <bartekhenry14@gmail.com>
2024-02-16 14:00:02 +0100

    Swagger installer

--- WorkoutPartner.Infrastructure/Configuration/Installer.cs
+++ WorkoutPartner.Infrastructure/Configuration/Installer.cs
@@ -19,6 +19,7 @@ public static class Installer
         Environment environment)
     {
         return serviceCollection
+            .InstallSwagger()
             .InstallDatabase(configuration, environment)
             .InstallRepositories()
             .InstallServices()
---
+++ WorkoutPartner.Infrastructure/Configuration/SwaggerInstaller.cs
@@ -0,0 +1,41 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace WorkoutPartner.Infrastructure.Configuration;
+
+public static class SwaggerInstaller
+{
+    public static IServiceCollection InstallSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(opt =>
+        {
+            opt.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+
+        return services;
+    }
+}
