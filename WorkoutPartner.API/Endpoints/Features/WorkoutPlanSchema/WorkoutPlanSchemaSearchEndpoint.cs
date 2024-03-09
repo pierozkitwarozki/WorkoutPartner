e81505e2396d5b1e9989c92cec8c17025d40316c
Bartłomiej <bartekhenry14@gmail.com>
2024-03-09 09:11:27 +0100

    WorkoutPlanSearch endpoint works

--- WorkoutPartner.API/Endpoints/Features/WorkoutPlanSchema/WorkoutPlanSchemaSearchEndpoint.cs
+++ WorkoutPartner.API/Endpoints/Features/WorkoutPlanSchema/WorkoutPlanSchemaSearchEndpoint.cs
@@ -1,27 +1,32 @@
+using System.Security.Claims;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using WorkoutPartner.API.Endpoints.Configuration;
+using WorkoutPartner.Application.Extensions;
 using WorkoutPartner.Application.Queries;
 using WorkoutPartner.Domain.DTO.Queries.ExerciseSearch;
+using WorkoutPartner.Domain.DTO.Queries.WorkoutPlanSchemaSearch;
 using WorkoutPartner.Domain.Routes;
 using WorkoutPartner.Infrastructure.Extensions;
 
-namespace WorkoutPartner.API.Endpoints.Features.Exercise;
+namespace WorkoutPartner.API.Endpoints.Features.WorkoutPlanSchema;
 
-public sealed class ExerciseSearchEndpoint : IEndpointBase
+public class WorkoutPlanSchemaSearchEndpoint : IEndpointBase
 {
-    public string Group => RouteGroupNames.Exercise;
+    public string Group => RouteGroupNames.WorkoutPlanSchema;
     public string Route => RouteNames.Search;
     public RouteHandlerBuilder MapEndpoint(RouteGroupBuilder builder)
     {
         return builder.MapGet(Route,
                 async (
-                    [AsParameters] ExerciseSearchRequest request,
+                    [AsParameters] WorkoutPlanSchemaSearchRequest request,
+                    ClaimsPrincipal user,
                     [FromServices] IMediator mediator) =>
                 {
-                    var query = new ExerciseSearchQuery
+                    var query = new WorkoutPlanSchemaSearchQuery
                     {
-                        Request = request
+                        Request = request,
+                        UserId = user.GetUserNameIdentifier()
                     };
 
                     var result = await mediator.Send(query);
